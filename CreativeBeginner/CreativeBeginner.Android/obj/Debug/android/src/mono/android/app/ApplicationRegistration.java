4e4a893d7dfa7c71e59ae9510cb079560d8877eb
Diego Ramos <31499304+IngDiegoRamos@users.noreply.github.com>
2018-09-10 20:38:45 -0500

    Commit

---
+++ CreativeBeginner/CreativeBeginner.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("CreativeBeginner.Droid.MainApplication, CreativeBeginner.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md56fa694416a2d29a4b92d6dd8b950ac73.MainApplication.class, md56fa694416a2d29a4b92d6dd8b950ac73.MainApplication.__md_methods);
+		
+	}
+}
